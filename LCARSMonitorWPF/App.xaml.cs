170e88960065bdd13b84118744b0913632e2ce38
Rewasvat <rewasvat@gmail.com>
2023-03-05 18:38:28 -0300

    Added the icon as the MainWindow icon; started implementation of tray icon

--- LCARSMonitorWPF/App.xaml.cs
+++ LCARSMonitorWPF/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace LCARSMonitorWPF
 {
@@ -13,5 +17,13 @@ namespace LCARSMonitorWPF
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon? trayIcon;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            trayIcon = new TaskbarIcon();
+            trayIcon.IconSource = new BitmapImage(new Uri("pack://application:,,,/Assets/Icons/lcars.png"));
+            trayIcon.ToolTipText = "LCARS Monitor";
+        }
     }
 }
