502295a5693fd5455e58b5cd96ae258bf42de4e9
Rewasvat <rewasvat@gmail.com>
2023-03-09 19:51:16 -0300

    Implemented some basic initial commands (still missing serialization and their support on controls)

---
+++ LCARSMonitorWPF/LCARS/Commands/ChangeBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LCARSMonitorWPF.Controls;
+
+namespace LCARSMonitorWPF.LCARS.Commands
+{
+    public class ChangeBoard : ILCARSCommand
+    {
+        public string TargetBoard { get; set; }
+        public string BoardToSelect { get; set; }
+
+        public ChangeBoard(string targetBoard, string boardToSelect)
+        {
+            TargetBoard = targetBoard;
+            BoardToSelect = boardToSelect;
+        }
+
+        public void OnRun()
+        {
+            Board target = new Board(); // TODO: FIX THIS get Board with TargetBoard name
+            target.CurrentBoard = BoardToSelect;
+        }
+    }
+}
---
+++ LCARSMonitorWPF/LCARS/Commands/SystemExecute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LCARSMonitorWPF.LCARS.Commands
+{
+    public class SystemExecute : ILCARSCommand
+    {
+        public string Command { get; set; }
+
+        public SystemExecute(string command)
+        {
+            Command = command;
+        }
+
+        public void OnRun()
+        {
+            Process.Start(Command);
+        }
+    }
+}
