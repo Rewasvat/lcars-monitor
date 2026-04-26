97b8a1d19870c581982745daf9cdfa0fd7d805db
Rewasvat <rewasvat@gmail.com>
2026-04-26 15:35:36 -0300

    ComputerSystem: - added is_active property - added selected_source property, that closes/opens system accordingly when changing active sensor source - open() now properly checks active source availability before initializing it, with logging issues to output - implemented render_menu() method to draw imgui controls to see/edit settings from ComputerSystem and its available sources. - implemented save/load persisted data from the System and all available sources into DataCache - refactored on-shutdown callback into its own method

--- lcarsmonitor/sensors/sensors.py
+++ lcarsmonitor/sensors/sensors.py
@@ -1,19 +1,17 @@
 import click
 import threading
 import libasvat.command_utils as cmd_utils
+import libasvat.imgui.editors.primitives as primitives
 from typing import Callable
-from typing import Iterator, TYPE_CHECKING
+from typing import Iterator
 from imgui_bundle import imgui
 from libasvat.data import DataCache
 from libasvat.imgui.colors import Colors
 from libasvat.imgui.general import adv_button
 from libasvat.imgui.editors import TypeDatabase, TypeEditor
+from libasvat.imgui.editors.controller import render_all_properties, get_all_prop_values_for_storage, restore_prop_values_to_object
 from lcarsmonitor.sensors.sensors_api import SensorSource, Hardware, InternalSensor, SensorID
 from lcarsmonitor.sensors.sources.dummy_impl import DummySensors
-
-
-if TYPE_CHECKING:
-    from lcarsmonitor.sensors.sensor_node import Sensor
 
 
 class ComputerSystem(metaclass=cmd_utils.Singleton):
@@ -34,17 +32,54 @@ class ComputerSystem(metaclass=cmd_utils.Singleton):
         self._lock: threading.Lock = None
 
         cache = DataCache()
-        cache.add_shutdown_listener(self.close)
+        cache.add_shutdown_listener(self._on_shutdown)
 
         self._available_sources = {name: source() for name, source in SensorSource.get_all_subclasses().items()}
-        self._selected_source: str = "LibreComputer"
-        # TODO: load and save selected-source from cache
+        self._selected_source: str = "None"
         self._dummy_source = DummySensors()
 
+        # Load stored data from cache
+        data: dict = cache.get_data("computersystem_data", {}).copy()
+        sources_data: dict = data.pop("sources_data", {})
+        restore_prop_values_to_object(self, data)
+        for source_name, source_data in sources_data.items():
+            source = self._available_sources.get(source_name)
+            if source and source_data:
+                source.load_data(source_data)
     @property
     def current_source(self):
         """Gets the currently selected SensorSource instance."""
         return self._available_sources.get(self._selected_source)
+
+    @primitives.string_property(options=["None"])
+    def selected_source(self) -> str:
+        """Name of the currently active SensorSource [GET/SET]
+
+        Note that when setting this value to change the active SensorSource, this may trigger a shutdown of the previous source
+        and initialization of newly selected source - both operations may be resource-intensive and affect the available sensors.
+
+        Also, if the selected source is invalid or not available, we'll still initialize, but only with the dummy sensors available.
+
+        If, for example, the selected source is unavailable due to some settings, in order to "re-initialize" the source you'll
+        need to select a different source, then go back to select the one you wanted.
+        """
+        return self._selected_source
+
+    @selected_source.setter
+    def selected_source(self, value: str):
+        if value == self._selected_source:
+            return
+        was_initialized = self.is_active
+        if was_initialized:
+            self.close()
+        self._selected_source = value
+        if was_initialized:
+            self.open()
+
+    @property
+    def is_active(self):
+        """Checks if the ComputerSystem was opened."""
+        return len(self.all_sensors) > 0
 
     def open(self, dummy_test=False):
         """Starts the computer object, allowing us to query its hardware, sensors and more.
@@ -53,22 +88,28 @@ class ComputerSystem(metaclass=cmd_utils.Singleton):
             dummy_test (bool, optional): If true, will use a dummy internal Computer object, with dummy sensors that simulate actual
                 sensors with changing values, for testing sensor-related code without needing to load sensors properly. Defaults to False.
         """
-        if len(self.all_sensors) > 0:
+        if self.is_active:
             click.secho("ComputerSystem: tried to open() while already opened.", fg="yellow")
             return
-        # TODO: a flag de dummy-test é pra desabilitar usar qualquer SensorSource, portanto rodando só com os DummySensors
-        if self.current_source:
-            self.current_source.initialize()
+        is_ok, error_msg = False, "invalid selected source"
+        if dummy_test:
+            is_ok = True
+            error_msg = "dummy-test enabled, using only dummy sensors"
+        elif self.current_source:
+            is_ok, error_msg = self.current_source.check_availability()
+            if is_ok:
+                self.current_source.initialize()
         self._dummy_source.initialize()
         self.all_sensors = {sensor.id: sensor for sensor in self.get_all_isensors()}
-        click.secho("ComputerSystem: Initialized!", fg="magenta")
+        msg_color = "green" if is_ok else "yellow"
+        click.secho(f"ComputerSystem: Initialized with Source '{self._source_name}': {click.style(error_msg, fg=msg_color)}!", fg="magenta")
         self.start_async_update()
 
     def close(self):
         """Stops the computer object, releasing resources.
 
         This is automatically called on shutdown of LCARSMonitor."""
-        if len(self.all_sensors) <= 0:
+        if not self.is_active:
             click.secho("ComputerSystem: tried to close() while already closed.", fg="yellow")
             return
         self.stop_async_update()
@@ -76,7 +117,15 @@ class ComputerSystem(metaclass=cmd_utils.Singleton):
         if self.current_source:
             self.current_source.shutdown()
         self._dummy_source.shutdown()
-        click.secho("ComputerSystem: Closed Computer.", fg="magenta")
+        click.secho(f"ComputerSystem: Closed Computer (source: {self._source_name}).", fg="magenta")
+
+    @property
+    def _source_name(self):
+        """Stylized pretty-name of the selected current-source for printing."""
+        if self.current_source:
+            return click.style(self.current_source.pretty_name, fg="green")
+        else:
+            return click.style("N/A", fg="red")
 
     def update(self):
         """Updates our hardware, to update all of our sensors.
@@ -147,12 +196,66 @@ class ComputerSystem(metaclass=cmd_utils.Singleton):
         """
         return self.all_sensors.get(id_obj)
 
+    def render_menu(self):
+        """Renders the IMGUI controls for showing ComputerSystem status and settings editors."""
+        imgui.separator_text("Computer/Sensor Settings")
+        render_all_properties(self)
+
+        if self.is_active:
+            num_sensors = len(self.all_sensors)
+            imgui.text_colored(Colors.green, f"Status: System active with {num_sensors-2} sensors (+2 dummy/test sensors).")
+        else:
+            imgui.text_colored(Colors.red, "Status: System not activated. Select a source to activate.")
+
+        prev_source_id = self._selected_source
+        tooltip = "Change the currently active Sensor Source to this."
+        for source_id, source in self._available_sources.items():
+            if imgui.collapsing_header(source.pretty_name):
+                imgui.text_wrapped(type(source).__doc__)
+                source.render_editor()
+                is_current = source_id == prev_source_id
+                is_ok, error_msg = source.check_availability()
+                can_select = (not is_current) and is_ok
+                imgui.text("Source Status:")
+                imgui.same_line()
+                msg_color = Colors.green if is_ok else Colors.red
+                imgui.push_style_color(imgui.Col_.text, msg_color)
+                imgui.text_wrapped(error_msg)
+                imgui.pop_style_color()
+                if adv_button("Select this Source", tooltip, is_enabled=can_select):
+                    self.selected_source = source_id
+                if is_current:
+                    imgui.same_line()
+                    imgui.text_colored(Colors.green, "[ACTIVE]")
+
+    def save(self):
+        """Saves persisted data from the ComputerSystem and its available Sensor Sources to the DataCache."""
+        cache = DataCache()
+        data = get_all_prop_values_for_storage(self)
+        sources_data = {}
+        for source_name, source in self._available_sources.items():
+            sources_data[source_name] = source.get_data()
+        data["sources_data"] = sources_data
+        cache.set_data("computersystem_data", data)
+
+    def _on_shutdown(self):
+        """Application shutdown callback"""
+        if self.is_active:
+            self.close()
+        self.save()
+
     def __iter__(self) -> Iterator[Hardware]:
         all_hw = []
         if self.current_source:
             all_hw.extend(self.current_source.get_all_hardware())
         all_hw.extend(self._dummy_source.get_all_hardware())
         return iter(all_hw)
+
+    def _update_selected_source_editor(self, editor: primitives.StringEditor):
+        """Update callback for our `selected_source` property editor."""
+        source_options = ["None (only Dummy sensors)"]
+        source_options.extend(self._available_sources.keys())
+        editor.options = source_options
 
 
 @TypeDatabase.register_editor_for_type(SensorID)
